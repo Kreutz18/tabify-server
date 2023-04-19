1b8acfdf246969ad367f8091ee09379c75fc538b
Jesse <JesseKKreutzberger@gmail.com>
2023-04-18 21:32:16 -0400

    tried to get tabs to work

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const puppeteer = require('puppeteer');
+const utils = require('../utils');
 
 const app = express();
 
@@ -12,14 +13,16 @@ app.get('/api/chords', async (req, res) => {
 app.get('/api/tabs', async (req, res) => {
   const lyrics = await scrapeUG(req.query.url);
   res.json({lyrics: lyrics});
-})
+});
 
-
+app.get('/api/tabs/search', async (req, res) => {
+  const tabs = await utils.searchTabs(req.query.title);
+  res.json({tabs: tabs});
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
 
 async function scrapeUG(url) {
   const browser = await puppeteer.launch();
@@ -32,5 +35,21 @@ async function scrapeUG(url) {
 
   await page.close();
 
-  return await lyrics;
+  return lyrics;
 }
+
+
+
+
+/**
+  check lIKMM PdXKy for type of tab
+  - remove offical/pro tabs
+  - pull href from <a> tag (className = aPPf7 jtEAE lBssT)
+  - pull innerText for songName (className = aPPf7 jtEAE lBssT) (innerText)
+  pull rating from song (className = djFV9)
+  pull type (tab/chords) (className = lIKMM PdXKy)
+  - two separate lists
+    6-string (chords/tabs)
+    Bass tabs
+  song name = className(aPPf7 HT3w5 lBssT) - innerText
+ */
