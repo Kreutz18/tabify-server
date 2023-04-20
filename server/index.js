8faaba6f9ac5a4e659ffbdb1a6756f11a668ebdf
Jesse <JesseKKreutzberger@gmail.com>
2023-04-20 19:13:55 -0400

    commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const puppeteer = require('puppeteer');
-const utils = require('../utils');
+const { JSDOM } = require('jsdom');
+
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.get('/api/tabs', async (req, res) => {
 });
 
 app.get('/api/tabs/search', async (req, res) => {
-  const tabs = await utils.searchTabs(req.query.title);
+  const tabs = await searchTabs(req.query.title);
   res.json({tabs: tabs});
 });
 
@@ -36,6 +37,65 @@ async function scrapeUG(url) {
   await page.close();
 
   return lyrics;
+}
+
+ async function searchTabs(title) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  let url = 'https://www.ultimate-guitar.com/search.php?search_type=title&value=';
+  url += title;
+
+  await page.goto(url);
+  const tabs = await page.$$eval("body > div.js-page.js-global-wrapper > div.fbcII > main > div.BDmSP > div.XwpqK > section > article > div > div.LQUZJ", (tabList) => {
+    return tabList.map(x => x.innerHTML);
+  });
+
+  await page.close();
+
+    let newTabs = tabs.map((x) => {
+      let tab = {
+          href: null,
+          songName: '',
+          type: null,
+          rating: 0
+        };
+
+        let TAB_TYPE = {
+          OFFICIAL: 'Official',
+          PRO: 'Pro',
+          CHORDS: 'Chords',
+          TAB: 'Tab',
+          BASS: 'Bass'
+        };
+        const document = new JSDOM(x.innerHtml).window.document;
+        let aTag = document.getElementsByClassName('aPPf7 jtEAE lBssT')[0];
+        console.log(aTag);
+        tab.href = aTag.href;
+        tab.songName = aTag.innerText;
+
+        let rating = document.getElementsByClassName('djFV9').innerText;
+        tab.rating = rating;
+
+        let type = document.getElementsByClassName('lIKMM PdXKy')[0];
+        switch (type.innerText) {
+          case TAB_TYPE.CHORDS:
+            tab.type = type.innerText;
+            break;
+          case TAB_TYPE.TAB:
+            tab.type = type.innerText;
+            break;
+          case TAB_TYPE.BASS:
+            tab.type = type.innerText;
+            break;
+          case TAB_TYPE.OFFICIAL:
+          case TAB_TYPE.PRO:
+          default: return;
+        }
+
+        return tab;
+    });
+
+  return newTabs;
 }
 
 
