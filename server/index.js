3079a1bb2e69b91ef1ec8a5c589c8b7e861653d8
Jesse <JesseKKreutzberger@gmail.com>
2023-04-23 15:01:32 -0400

    added waitUntil for tabs

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.listen(PORT, () => {
 async function scrapeUG(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(url, {waitUntil: 'networkidle2', timeout: 10000});
 
   const lyrics = await page.$$eval("body > div.js-page.js-global-wrapper > div.fbcII > main > div.BDmSP > article.o2tA_.JJ8_m > section:nth-child(2) > article > section.P8ReX", (lyrics) => {
     return lyrics.map(x => x.textContent);
